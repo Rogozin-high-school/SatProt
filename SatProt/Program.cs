5eaa9c28606e67e49616dcce395045523e67d712
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-18 19:36:03 +0200

    All the rest

---
+++ SatProt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatProt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server s = new Server(8080);
+            s.MessageHandler = (b) =>
+            {
+                InMsg msg = new InMsg(b);
+                int x = msg.NextInt();
+                ;
+            };
+            s.Start();
+        }
+    }
+}
